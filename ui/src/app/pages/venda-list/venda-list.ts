f0448741ec32c3dc1f3495abf4f939aa4529a2ac
Leo Martelli <l.martelli@aluno.ifsp.edu.br>
2026-04-30 08:52:22 -0300

    filtro e ordenacao da table

--- ui/src/app/pages/venda-list/venda-list.ts
+++ ui/src/app/pages/venda-list/venda-list.ts
@@ -1,20 +1,30 @@
 import { Component, inject } from '@angular/core';
 import { VendaService } from '../../services/venda.service';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, DatePipe } from '@angular/common';
 import { Table, TableModule } from 'primeng/table';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputIconModule } from 'primeng/inputicon';
+import { Observable } from 'rxjs';
+import { Venda } from '../../modelos/venda.modelo';
 
 @Component({
   selector: 'app-venda-list',
-  imports: [AsyncPipe, TableModule],
+  imports: [AsyncPipe, TableModule, DatePipe, IconFieldModule, InputTextModule, InputIconModule],
   templateUrl: './venda-list.html',
   styleUrl: './venda-list.scss',
 })
 export class VendaList {
   private vendasService = inject(VendaService);
-  // protected vendas$ = new Observable<Venda[]>(); //normalmente
-  protected vendas$ = this.vendasService.getAll(); //teste
+  protected vendas$ = new Observable<Venda[]>(); //normalmente
+  // protected vendas$ = this.vendasService.getAll(); //teste
+  protected selectedVenda$: any = {};
 
   constructor() {
+    this.obterVendas();
+  }
 
+  protected obterVendas() {
+    this.vendas$ = this.vendasService.getAll();
   }
 }
